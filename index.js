d3cbb7c7f93a6e8ed3bd82c18d21da667151cdb9
a01382811 <a01382811@gmail.com>
2026-04-17 18:06:39 +0100

    Add index.js with initial git commands
    Add index.js and include git commands for setup.

---
+++ index.js
@@ -0,0 +1,9 @@
+cd KILLER-MD-GOD
+
+# Vérifier que index.js existe
+ls -la index.js
+
+# Ajouter tous les fichiers
+git add .
+git commit -m "Ajout de index.js et tous les fichiers"
+git push origin main
